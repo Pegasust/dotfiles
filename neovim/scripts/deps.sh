0bdbb733a2384dcef82a288db7a6f20d06596b83
pegasust <pegasucksgg@gmail.com>
2022-10-11 17:13:07 -0700

    add dependencies installs

---
+++ neovim/scripts/deps.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# This file installs dependencies needed for the Neovim plugins
+echo "Please run this in sudo mode for sudo apt* commands"
+
+# Pip and Python3
+PYTHON_3=${PYTHON_3:-"python3.10"}
+apt install $PYTHON_3
+$PYTHON_3 -m ensurepip --upgrade
+$PYTHON_3 -m pip install --upgrade pip
+
+# Neovim vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
