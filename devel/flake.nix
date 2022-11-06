334a74dbb84aa2fd2497e8afd585b8bf781e0458
pegasust <pegasucksgg@gmail.com>
2022-11-06 12:56:42 +0000

    bad config, let's redo from star

--- devel/flake.nix
+++ devel/flake.nix
@@ -6,25 +6,18 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: {
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
-    };
-    programs.git = {
-      enable = true;
-    };
-    programs.zsh = {
-          enable = true;
-          shellAliases = {
-          	# list lists
-          	ll = "ls -l";
-          	update = "sudo nixos-rebuild switch";
-          };
-          history = {
-          	size = 10000;
-          	path = "${config.xdg.dataHome}/zsh/history";
-          };
-    };
-  };
+  outputs = { 
+    self, # instance of self
+    nixpkgs, # nixpkgs flake
+    flake-utils, 
+    home-manager,
+    ... 
+  }: flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in {
+            devShells.default = import ./shell.nix {
+	       inherit pkgs;
+	       inherit home-manager;
+	    };
+      }
+    );
 }
