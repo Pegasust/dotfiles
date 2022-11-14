af59d295cde57b565167aed67098d21e140f02ce
pegasust <pegasucksgg@gmail.com>
2022-11-14 18:01:39 +0000

    feat: add ssh/authorized_keys

--- system-nix/configuration.nix
+++ system-nix/configuration.nix
@@ -15,17 +15,38 @@ with lib;
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
+  users.users.hungtr = {
+    isNormalUser = true;
+    home = "/home/hungtr";
+    description = "pegasust/hungtr";
+    extraGroups = [ "wheel" "networkmanager" ];
+    openssh.authorizedKeys.keys = lib.strings.splitString "\n" (builtins.readFile ../ssh/authorized_keys);
+  };
+
+  # Let's just open ssh server in general, even though it may not be
+  # network-accessible
+  services.openssh = {
+    permitRootLogin = "no";
+    enable = true;
+  };
 
   # Some basic programs
   programs.neovim = {
     enable = true;
     defaultEditor = true;
   };
+
   programs.git = {
     enable = true;
     # more information should be configured under user level
   };
+
   environment.systemPackages = [
     pkgs.gnumake
+    pkgs.wget
+    pkgs.inetutils
+    pkgs.mtr
+    pkgs.sysstat
   ];
 }
+
