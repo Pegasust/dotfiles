19ef7307df1951b0f4e910f4ffc0ddd22d570854
pegasust <pegasucksgg@gmail.com>
2022-11-28 18:44:03 +0000

    nit

--- system-nix/profiles/nyx/hardware-configuration.nix
+++ system-nix/profiles/nyx/hardware-configuration.nix
@@ -15,7 +15,7 @@
 
   boot.kernelParams = ["console=ttyS0,19200n8"];
   boot.loader.grub.extraConfig = ''
-    serial --speed19200 --unit=0 --word=8 --parity=no --stop=1
+    serial --speed=19200 --unit=0 --word=8 --parity=no --stop=1
     terminal_input serial;
     terminal_output serial;
   '';
