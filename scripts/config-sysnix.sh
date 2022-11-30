88cde1a5b51ef45301f10121ecbd62957a4fda24
pegasust <pegasucksgg@gmail.com>
2022-11-29 19:58:57 -0700

    fix: create directory and add to version control so nix flake sees

--- scripts/config-sysnix.sh
+++ scripts/config-sysnix.sh
@@ -15,10 +15,13 @@ echo "SCRIPT_DIR: ${SCRIPT_DIR}"
 SYSNIX_DIR="${SCRIPT_DIR}/../system-nix"
 
 # Copy hardware-configuration of existing machine onto our version control
-HARDWARE_CONF="${SYSNIX_DIR}/profiles/${HOSTNAME}/hardware-configuration.nix" 
-if [ ! -f ${HARDWARE_CONF} ];
+SYSNIX_PROF="${SYSNIX_DIR}/profiles/${HOSTNAME}"
+HARDWARE_CONF="${SYSNIX_PROF}/hardware-configuration.nix" 
+if [ ! -f "${HARDWARE_CONF}" ]; then;
+	mkdir "$SYSNIX_PROF"
 	sudo cp /etc/nixos/hardware-configuration.nix ${HARDWARE_CONF}
 fi
+git add "${HARDWARE_CONF}"
 
 echo "Apply nixos-rebuild"
 sudo nixos-rebuild switch --flake "${SYSNIX_DIR}#${HOSTNAME}"
