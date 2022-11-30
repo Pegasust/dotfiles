bbd41b7272ee3afe1841a0bb93151a7c046d0612
pegasust <pegasucksgg@gmail.com>
2022-11-30 00:53:52 -0700

    pixi should be fixed as soon as possible, idk how this can play out between deploy and prod

--- system-nix/flake.nix
+++ system-nix/flake.nix
@@ -23,6 +23,59 @@
           };
         };
       };
+      nixosConfigurations.lizzi = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+        specialArgs = {
+          hostname = "lizzi";
+          _networking = {
+            interfaces.eth1.ipv4.addresses = [{
+              address = "71.0.0.1";
+              prefixLength = 24;
+            }];
+            firewall = {
+              enable = false;
+              allowedTCPPorts = [ 80 443 22 ];
+            };
+            useDHCP = false;
+            interfaces.eth0.useDHCP = true;
+          };
+          _boot.loader.grub.enable = true;
+          _boot.loader.grub.version = 2;
+          _services.openssh = {
+            permitRootLogin = "no";
+            enable = true;
+          };
+          _services.gitea = {
+            enable = true;
+            stateDir = "/gitea";
+            rootUrl = "https://git.pegasust.com";
+            settings = {
+              repository = {
+                "ENABLE_PUSH_CREATE_USER" = true;
+                "ENABLE_PUSH_CREATE_ORG" = true;
+              };
+            };
+          };
+          _services.nginx = {
+            enable = true;
+            clientMaxBodySize = "100m"; # Allow big file transfers over git :^)
+            recommendedGzipSettings = true;
+            recommendedOptimisation = true;
+            recommendedProxySettings = true;
+            recommendedTlsSettings = true;
+            virtualHosts."git.pegasust.com" = {
+              # Gitea hostname
+              sslCertificate = "/var/lib/acme/git.pegasust.com/fullchain.pem";
+              sslCertificateKey = "/var/lib/acme/git.pegasust.com/key.pem";
+              forceSSL = true; # Runs on port 80 and 443
+              locations."/".proxyPass = "http://localhost:3000/"; # Proxy to Gitea
+            };
+          };
+        };
+      };
       # Generic machine
       nixosConfigurations.pixi = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
