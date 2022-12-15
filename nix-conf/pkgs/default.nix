b441ee75c9ea41ebefaa7e421346db3e612070ea
pegasust <pegasucksgg@gmail.com>
2022-12-15 07:55:32 -0700

    add mypkgs

--- nix-conf/pkgs/default.nix
+++ nix-conf/pkgs/default.nix
@@ -1,8 +1,12 @@
 # This module aims to be merge (not inject/override) with top-level pkgs to provide
 # personalized/custom packages
-{ pkgs, lib, ... }@pkgs_input: {
+{ pkgs
+, lib
+, naersk # rust packages
+, ...
+}@pkgs_input: {
   # dot-hwtr = import "./dot-hwtr" pkgs_input;
-  cargo-bacon = pkgs.rustPlatform.buildRustPackage rec { 
-      pname = "bacon";
+  cargo-bacon = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "bacon";
   };
 }
