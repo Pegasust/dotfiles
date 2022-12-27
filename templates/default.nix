85dda3dbd3c75484ccb3417878cbf14d997ec3eb
Pegasust <pegasucksgg@gmail.com>
2022-12-26 21:51:32 -0700

    add some rust templates

--- templates/default.nix
+++ templates/default.nix
@@ -1,5 +1,15 @@
+# TODO: templates should be able to have initial states like
+# repo name, author,...
 {pkgs
 ,lib
-,
+,...
 }: {
+  rust = {
+    path = ./rust;
+    description = "Minimal Rust build template using Naersk, rust-overlay, rust-analyzer";
+  };
+  rust-monorepo = {
+    path = ./rust-monorepo;
+    description = "hungtr's opinionated Rust monorepo, extended from ./rust, using Cargo workspace";
+  };
 }
